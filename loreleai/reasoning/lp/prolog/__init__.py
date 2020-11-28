b5f21e7edf6f279bda585e04455ff257c2feb435
Sebastijan Dumancic <sebastijan.dumancic@gmail.com>
2020-11-28 17:32:05 +0100

    better dependency hanlding

--- loreleai/reasoning/lp/prolog/__init__.py
+++ loreleai/reasoning/lp/prolog/__init__.py
@@ -3,12 +3,32 @@
 # from .XSBProlog import XSBProlog
 # #from .Prolog import Prolog
 
-from pylo.engines.prolog import GNUProlog, SWIProlog, XSBProlog
-from pylo.engines.prolog.prologsolver import Prolog
+engines = []
 
-__all__ = [
-    "SWIProlog",
-    "XSBProlog",
-    "GNUProlog",
-    "Prolog"
-]
+try:
+    from pylo.engines.prolog import GNUProlog
+    engines += ['GNUProlog']
+except Exception:
+    pass
+
+try:
+    from pylo.engines.prolog import SWIProlog
+    engines += ['SWIProlog']
+except Exception:
+    pass
+
+try:
+    from pylo.engines.prolog import XSBProlog
+    engines += ['XSBProlog']
+except Exception:
+    pass
+
+engines += ['Prolog']
+__all__ = engines
+
+# __all__ = [
+#     "SWIProlog",
+#     "XSBProlog",
+#     "GNUProlog",
+#     "Prolog"
+# ]
