5b86ef8916f4b234c261865449464eb5ad9fe5aa
Sebastijan Dumancic <sebastijan.dumancic@gmail.com>
2021-02-17 09:37:14 +0100

    cache support

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='loreleai',
-      version='0.1.3',
+      version='0.1.4',
       description='A library for program induction/synthesis and StarAI',
       long_description=long_description,
       long_description_content_type='text/markdown',
